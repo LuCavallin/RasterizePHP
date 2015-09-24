71c06cfe0c81dc20001a64bd6bf56c3d47370da6
Luca Cavallin <me@lucavall.in>
2015-09-24 09:23:03 +0200

    now PSR-2 compliant!

--- RasterizePHP/RasterizePHPException.php
+++ RasterizePHP/RasterizePHPException.php
@@ -1,29 +1,32 @@
 <?php namespace LuCavallin\RasterizePHP;
 
-    use Exception;
+use Exception;
+
+/**
+ * Class RasterizePHPException
+ * @package LuCavallin\RasterizePHP
+ *
+ * Custom exceptions class for RasterizePHP
+ */
+class RasterizePHPException extends Exception
+{
 
     /**
-     * Class RasterizePHPException
-     * @package LuCavallin\RasterizePHP
-     *
-     * Custom exceptions class for RasterizePHP
+     * @param string $message
+     * @param int $code
+     * @param Exception|null $previous
      */
-    class RasterizePHPException extends Exception {
-
-        /**
-         * @param string $message
-         * @param int $code
-         * @param Exception|null $previous
-         */
-        public function __construct($message, $code = 0, Exception $previous = null) {
-            parent::__construct($message, $code, $previous);
-        }
-
-        /**
-         * @return string
-         */
-        public function __toString() {
-            return __CLASS__ . ": {$this->message}\n";
-        }
-
+    public function __construct($message, $code = 0, Exception $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
     }
+
+    /**
+     * @return string
+     */
+    public function __toString()
+    {
+        return __CLASS__ . ": {$this->message}\n";
+    }
+
+}
